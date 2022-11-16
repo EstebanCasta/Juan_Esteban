3edaa85df914180924941e969c881b8460139cde
EstebanCasta <113556462+EstebanCasta@users.noreply.github.com>
2022-11-16 15:52:54 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+.window.addEventListener('load', () => {
+    const contendor_puntos = document.querySelector('.pagina-cargando')
+    contendor_puntos.style.opacity = 0
+    contendor_puntos.style.visibility = 'hidden'
+})
